c7190603978d98bd1a513e34351fadb48ade4bc5
admin <admin@example.com>
2026-06-09 23:42:26 +0800

    fix: 补充 dbinbound.js 中 address 字段对 IPv6 通配符 :: 的处理

--- web/assets/js/model/dbinbound.js
+++ web/assets/js/model/dbinbound.js
@@ -74,7 +74,7 @@ class DBInbound {
 
     get address() {
         let address = location.hostname;
-        if (!ObjectUtil.isEmpty(this.listen) && this.listen !== "0.0.0.0") {
+        if (!ObjectUtil.isEmpty(this.listen) && this.listen !== "0.0.0.0" && this.listen !== "::") {
             address = this.listen;
         }
         return address;
